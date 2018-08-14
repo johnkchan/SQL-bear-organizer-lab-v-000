621e36cbd7898b5eaab98bae340435b7b4fdb1f9
Jonathan Chan <johnkchan252@gmail.com>
2018-08-14 03:43:58 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 1, M, Brown, Chill, T)
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Rowdy, 1, M, Brown, Chill, T)
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 1, M, Brown, Chill, T)
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Tabitha, 1, M, Brown, Chill, T)
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 1, M, Brown, Chill, T)
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 1, M, Brown, Chill, T)
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (Mr. Chocolate, 1, M, Brown, Chill, T)
